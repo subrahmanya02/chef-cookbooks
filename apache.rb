90ad1ce59e82a3461c2fc37f77a877d46d9f5ff9
subrahmanya02 <subrahmanya02@gmail.com>
2021-10-01 12:19:31 +0530

    c

---
+++ apache.rb
@@ -0,0 +1,10 @@
+#Install and enable apache
+execute "apt-get update" do
+  command "apt-get update"
+end
+package "apache2" do
+  action :install
+end
+service "apache2" do
+  action [:enable, "start]
+end  
